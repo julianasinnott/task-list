7795822bd22c5c7ab740842ea1ab8528b39b2936
julianasinnott <julianasinnott@outlook.com>
2022-05-16 01:20:11 -0300

    creating task list

--- src/pages/Initial/index.jsx
+++ src/pages/Initial/index.jsx
@@ -1,23 +1,25 @@
+import { useState } from 'react'
 import { Card } from '../../components/Card'
 import './styles.css'
 
-
-
-
-
-
-
-
 export function Initial() {
-
+  const [tasksName, setTasksName] = useState('') 
+  const [taskList, setTaskList] = useState([])
 
   return (
-    
     <div className='box'>
-      <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Qual seu nome?"/>
-      <button type="button">Adicionar</button>
-      <Card />
+      <h1>Lista de Tarefas</h1>
+      <input
+        onChange={e => setTasksName(e.target.value)}
+        type="text"
+        placeholder="Qual seu nome?"/>
+      <button
+        type="button"
+        onClick={() => setTaskList([...taskList, tasksName])}
+      >
+        Adicionar
+      </button>
+      { taskList.map((task, index) => <Card key={index} nameTask={task} />) }
     </div>
   )
 }
