195940800a466affdf6c508f6536b26707ee1cb1
Juliana <julianasinnott@outlook.com>
2022-06-11 01:51:33 -0300

    form update, local Storage add

--- src/pages/Initial/index.jsx
+++ src/pages/Initial/index.jsx
@@ -7,20 +7,28 @@ import { useGetUser } from '../../services/useGetUser'
 import './styles.css'
 
 export function Initial() {
+  const initialTaskList = JSON.parse(localStorage.getItem('taskStorage'))  
   const [tasksName, setTasksName] = useState('') 
-  const [taskList, setTaskList] = useState([])
+  const [taskList, setTaskList] = useState(initialTaskList || [])
   const { data, getUser } = useGetUser()
 
   useEffect(() => {
     getUser()
   }, [])
 
+  useEffect(()=>{
+    setItemOnLocalStorage('taskStorage', taskList)
+  },[taskList])
+
+  function setItemOnLocalStorage(key, value) {
+    localStorage.setItem(key,JSON.stringify(value)) 
+  } 
+
   function taskSubmit(event) {   
     event.preventDefault()
     tasksName !== '' && setTaskList([...taskList, tasksName])    
-    Array.from(document.querySelectorAll("input")).forEach(
-      input => (input.value = ""))
-  }
+    event.target.reset()
+  } 
 
   function resetTaskList (event){
     event.preventDefault()
@@ -32,19 +40,20 @@ export function Initial() {
       <Header user={data} />
       <h1>Lista de Tarefas</h1>
       <main>
-        <form className='form'>
+        <form onSubmit={taskSubmit} className='form'>
           <input
             onChange={e => setTasksName(e.target.value)}
             type="text"
             placeholder="O que vamos fazer hoje?"
           />
           <button
+            type="submit"
             className="button"
-            onClick={taskSubmit}  
           >
             <ListPlus size={24} />
           </button>
           <button
+            type="reset"
             className="button"
             onClick={resetTaskList}
           >
